a1e4ed38e2d664423ba8abcf895bff0a3978ae58
Kasia <kasia@miroartdesign.com>
2019-04-29 16:54:47 -0400

    drills

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
 "use strict"
-const myGreeting = function(name ,age) {
-    if (!name || !age) {
-        throw Error('Arguments not valid');
-    }
-    if (typeof name !== 'string' || typeof age !== 'number') {
-        throw TypeError();
-    }
-    return `Hi my name is ${name} and I'm ${age}`;
+// const myGreeting = function(name ,age) {
+//     if (!name || !age) {
+//         throw Error('Arguments not valid');
+//     }
+//     if (typeof name !== 'string' || typeof age !== 'number') {
+//         throw TypeError();
+//     }
+//     return `Hi my name is ${name} and I'm ${age}`;
+// };
+
+
+// // try {
+// //     console.log(myGreeting());
+// // } catch (error) {
+// //     console.log(error);
+// // };
+
+// const greeting1 = myGreeting('Chris', '28');
+
+// const yearOfBirth = function(age) {
+//     if (age < 0) {
+//         throw Error("Age cannot be negative");
+//     }
+//     const YOB = 2019 - age;
+//     return `I was born in ${YOB}`;
+// };
+
+
+
+//Jedi
+function jediName(firstName, lastName){
+    return lastName.slice(0,3) + firstName.slice(0,2);
 }
+console.log(jediName('kasia','mirowska'));
 
+//infinite
 
-try {
-    console.log(myGreeting());
-} catch (error) {
-    console.log(error);
-}
-
-const greeting1 = myGreeting('Chris', '28');
-
-const yearOfBirth = function(age) {
-    if (age < 0) {
-        throw Error("Age cannot be negative");
+function beyond(num) {
+    if (num === Infinity) {
+        console.log('And beyond');
     }
-    const YOB = 2019 - age;
-    return `I was born in ${YOB}`;
-}
+    if (num > 0){
+        console.log('To infinity');
+    }
+    if (num < 0){
+        console.log('To negative infinity');
+    }
+    if (num === 0){
+        console.log('Staying home');
+    }
+};
+console.log(beyond());
 
-console.log(' blabla')
+
+//decoding
+function decode(str){
+let strArr = str.split(' ');
+console.log(strArr);
+let decodedWord = [];
+for ( let i = 0; i< strArr.length; i ++){
+    if ( strArr[i][0] === 'a'){
+        decodedWord.push(strArr[i][1]);
+        }
+    if ( strArr[i][0] === 'b' ){
+        decodedWord.push(strArr[i][2]);
+    }
+    if (strArr[i][0] === 'c'){
+        decodedWord.push(strArr[i][3]);
+    }
+    if (strArr[i][0] === 'd'){
+        decodedWord.push(strArr[i][4]);
+    }
+    else {
+        decodedWord.push(' ');
+    }
+    
+}
+return decodedWord.join('');
+};
+console.log(decode('craft block argon meter bells brown croon droop'));
