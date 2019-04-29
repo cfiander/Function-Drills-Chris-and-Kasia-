324c71662c40a0398c8d0b7b86e3a63712c1fa07
cfiander <cfiander90@gmail.com>
2019-04-29 13:05:46 -0700

    final

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 "use strict"
 const myGreeting = function(name ,age) {
+    if (!name || !age) {
+        throw Error('Arguments not valid');
+    }
+    if (typeof name !== 'string' || typeof age !== 'number') {
+        throw TypeError();
+    }
     return `Hi my name is ${name} and I'm ${age}`;
 }
 
-console.log(myGreeting('Kasia'));
+try {
+    console.log(myGreeting());
+} catch (error) {
+    console.log(error);
+}
+
+const greeting1 = myGreeting('Chris', '28');
+
+const yearOfBirth = function(age) {
+    if (age < 0) {
+        throw Error("Age cannot be negative");
+    }
+    const YOB = 2019 - age;
+    return `I was born in ${YOB}`;
+}
+
+
